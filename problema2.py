004ba6e552002ee9a6a27dd2de64423117239665
franciscoo30 <137561858+franciscoo30@users.noreply.github.com>
2024-03-22 10:06:26 -0300

    Add files via upload

--- problema2.py
+++ problema2.py
@@ -7,5 +7,10 @@
 #         20 90 hola jiji 77
 #La salida debe ser
 #         (77, 'jiji', 'hola', 90, 20)
-t = input()
-print(t)
+
+t = input().split()
+
+tupla_convertida = tuple(int(x) if x.isdigit() else x for x in t)
+
+tupla_invertida = tupla_convertida[::-1]
+print(tupla_invertida)
