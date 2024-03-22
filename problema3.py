66f656bfa53626ee77ca7dbfd63b2a8ac8e6dd27
franciscoo30 <137561858+franciscoo30@users.noreply.github.com>
2024-03-22 10:43:44 -0300

    Add files via upload

--- problema3.py
+++ problema3.py
@@ -9,5 +9,39 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+
+def arbolTrinario(numero):
+	return [numero, [], [], []]
+
+def insertaEnArbolTrinario(arbol,numero):
+	if arbol==[]:
+		arbol+=arbolTrinario(numero)
+	elif numero < arbol[0]:
+		insertaEnArbolTrinario(arbol[1],numero)
+	elif numero == arbol[0]:
+		insertaEnArbolTrinario(arbol[2],numero)
+	else:
+		insertaEnArbolTrinario(arbol[3],numero)
+
+def estaEnArbolTrinario(arbol,numero):
+	if arbol==[]:
+		return False
+	elif numero==arbol[0]:
+		return True	
+	elif numero<arbol[0]:
+		return estaEnArbolTrinario(arbol[1],numero)
+	else:
+		return estaEnArbolTrinario(arbol[3],numero)
+
+lista = input().split()
+print(lista)
+
+lista2 = [int(x) if x.isdigit() else x for x in lista]
+
+arbol = arbolTrinario(lista2[0])
+
+for x in lista2:
+	insertaEnArbolTrinario(arbol, x)
+
+print(arbol)
+
